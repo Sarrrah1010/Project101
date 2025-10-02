be4f42fa883218035ea8cdce1d96adb428afa46c
786 <786@SARAH>
2025-10-02 06:10:47 +0500

    Add project files.

---
+++ Project101/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Project101
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Hello, World!");
+
+        
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        
+        Console.WriteLine("Nice to meet you, " + name + "!");
+
+       
+        Console.ReadKey();
+    }
+}
